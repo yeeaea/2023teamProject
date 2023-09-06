1282343303864a6af924276c3cf0817442bb3224
we1comerain <we1comerain@kakao.com>
2023-09-06 12:50:07 +0900

    css 수정

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -18,3 +18,22 @@ create table funeral(
     funeral_addr  VARCHAR(200) NOT NULL,
     funeral_time  VARCHAR(50)
 );
+
+drop table free_board;
+drop table ques_board;
+-- 자유게시판 정보 테이블 생성
+create table free_board(
+    free_no      bigint auto_increment NOT NULL PRIMARY KEY,
+    free_title   VARCHAR(200)   NOT NULL, 
+    free_content VARCHAR(2000) NOT NULL,
+    free_rdate   TIMESTAMP ,
+    free_udate   TIMESTAMP
+    );
+-- 질문 게시판 테이블 생성
+create table ques_board(
+    ques_no      bigint auto_increment NOT NULL PRIMARY KEY,
+    ques_title   VARCHAR(200)   NOT NULL, 
+    ques_content VARCHAR(2000) NOT NULL,
+    ques_rdate   TIMESTAMP,
+    ques_udate   TIMESTAMP 
+);
