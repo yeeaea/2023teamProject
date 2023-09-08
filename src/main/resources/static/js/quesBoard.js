423211a662433ff53d6bc3628e45d428efcac2c6
Sim-MinA <tlaalsdk7@naver.com>
2023-09-08 15:03:47 +0900

    삭제할 때 진짜 삭제할 것인지 묻는 창 띄우기 글 수정시 이미지를 다시 선택하지 않아도 이미지 들어가게 하고 이미지 삭제 버튼 만들기

--- src/main/resources/static/js/quesBoard.js
+++ src/main/resources/static/js/quesBoard.js
@@ -3,6 +3,8 @@ const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
 	deleteButton.addEventListener('click', event => {
+		// 확인 창을 띄우고 사용자의 선택 결과를 받습니다.
+		const isConfirmed = confirm('삭제하시겠습니까?');
 		let quesNo = document.getElementById('question-quesNo').value;
 		fetch(`/api/questions/${quesNo}`, {
 			method: 'DELETE'
@@ -23,12 +25,24 @@ if (modifyButton) {
 
     // 수정할 글의 ID (quesNo)를 가져옵니다.
     let quesNo = document.getElementById('question-quesNo').value;
+    
+    let title = document.getElementById("quesTitle").value;
+	let content = document.getElementById("quesContent").value;
 
     const formData = new FormData();
     formData.append("quesTitle", document.getElementById("quesTitle").value);
     formData.append("quesContent", document.getElementById("quesContent").value);
     formData.append("file", document.querySelector('input[type="file"]').files[0]);
-
+    
+    
+	if (title == "" && content == "") {
+		alert("제목과 내용을 입력해주세요!")
+	} else if (content == "") {
+		alert("내용을 입력해주세요!");
+	} else if (title == "") {
+		alert("제목을 입력해주세요!")
+	} else {
+    
     // URL에 실제 quesNo 값을 대체하여 요청을 보냅니다.
     fetch(`/api/questions/${quesNo}`, {
       method: "PUT",
@@ -46,109 +60,64 @@ if (modifyButton) {
         console.error("Error:", error);
         alert("수정 실패");
       });
+     }
   });
 }
 
+// 이미지 삭제하기
+document.addEventListener("DOMContentLoaded", function () {
+    // 이미지 삭제 버튼 클릭 시 이벤트 핸들러 추가
+    document.getElementById("deleteImg").addEventListener("click", function () {
+      var confirmDelete = confirm("이미지를 삭제하시겠습니까?");
+      if (confirmDelete) {
+           // 이미지 업로드 필드의 값을 지우기 (파일 선택 취소)
+           document.getElementById("imageUpload").value = "";
+           }
+    });
+});
 
-// 수정 기능2
-const modifyButton2 = document.getElementById("modify-btn2");
 
-if (modifyButton2) {
-	modifyButton2.addEventListener("click", event => {
-		let params = new URLSearchParams(location.search);
-		let quesNo = params.get("quesNo");
 
-		let title = document.getElementById("quesTitle").value;
-		let content = document.getElementById("quesContent").value;
-
-		if (title == "" && content == "") {
-			alert("제목과 내용을 입력해주세요!")
-		} else if (content == "") {
-			alert("내용을 입력해주세요!");
-		} else if (title == "") {
-			alert("제목을 입력해주세요!")
-		} else {
-			fetch(`/api/questions/${quesNo}`, {
-				method: 'PUT',
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					quesTitle: document.getElementById("quesTitle").value,
-					quesContent: document.getElementById("quesContent").value,
-				})
-			})
-				.then(() => {
-					alert("수정이 완료되었습니다.")
-					location.replace(`/questions/${quesNo}`);
-				});
-		}
-	});
-}
-
-// 생성 기능
+// 등록 기능
 const createButton = document.getElementById("create-btn");
-
 if (createButton) {
-  createButton.addEventListener("click", (event) => {
-    event.preventDefault(); // 기본 이벤트 방지
+   createButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      
+      const formData = new FormData();
+      formData.append("quesTitle", document.getElementById("quesTitle").value);
+      formData.append("quesContent", document.getElementById("quesContent").value);
+      formData.append("file", document.querySelector('input[type="file"]').files[0]);
+      
+      let title = document.getElementById("quesTitle").value;
+      let content = document.getElementById("quesContent").value;
 
-console.log("createButton 클릭 이벤트 발생");
-
-    const formData = new FormData();
-    formData.append("quesTitle", document.getElementById("quesTitle").value);
-    formData.append("quesContent", document.getElementById("quesContent").value);
-    formData.append("file", document.querySelector('input[type="file"]').files[0]);
-
-    fetch("/api/questions", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        if (response.status === 201) {
-          alert("등록 완료");
-          location.replace("/questions");
-        } else {
-          alert("등록 실패");
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("등록 실패");
-      });
-  });
-} 
-
-// 등록 기능2
-const createButton2 = document.getElementById("create-btn2");
-if (createButton2) {
-	createButton2.addEventListener("click", (event) => {
-		let title = document.getElementById("quesTitle").value;
-		let content = document.getElementById("quesContent").value;
-
-		if (title == "" && content == "") {
-			alert("제목과 내용을 입력해주세요!")
-		} else if (content == "") {
-			alert("내용을 입력해주세요!");
-		} else if (title == "") {
-			alert("제목을 입력해주세요!")
-		} else {
-			fetch("/api/questions", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					quesTitle: document.getElementById("quesTitle").value,
-					quesContent: document.getElementById("quesContent").value,
-				}),
-			}).then(() => {
-				alert("등록이 완료되었습니다.");
-				location.replace("/questions");
-			});
-		}
-	});
+      if (title == "" && content == "") {
+         alert("제목과 내용을 입력해주세요!")
+      } else if (content == "") {
+         alert("내용을 입력해주세요!");
+      } else if (title == "") {
+         alert("제목을 입력해주세요!")
+      } else {
+         fetch("/api/questions", {
+            method: "POST",
+            body: formData,
+         }).then((response) => {
+            if(response.status === 201){
+               alert("등록이 완료되었습니다.");
+               location.replace("/questions");   
+            }else{
+               alert("등록 실패");
+            }
+         })
+         .catch((error) => {
+            console.error("Error: ", error);
+            alert("등록 실패");
+         });
+      }
+   });
 }
+
 
 
 // 글 목록으로 가는 기능
