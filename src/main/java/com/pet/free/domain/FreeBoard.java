531c143038c4bdd1d31ed0436ee65b6a1bbcc6f6
Y0ungSe0 <rldyaldudtj@naver.com>
2023-09-09 16:47:27 +0900

    Merge branch 'master' into youngseo

--- src/main/java/com/pet/free/domain/FreeBoard.java
+++ src/main/java/com/pet/free/domain/FreeBoard.java
@@ -12,14 +12,14 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@Table(name="free_board")
 @Entity
 @Getter
 @Setter
--- src/main/resources/static/js/contentCount.js
+++ src/main/resources/static/js/contentCount.js
@@ -1,18 +1,23 @@
-// 글 작성하기에서 내용 2000자 못넘게
+// 글 작성하기에서 내용 1000자 못넘게
 $('#quesContent').on('keydown', function() {
     var content = $(this).val();
     if (content.length > 1000) {
         alert("최대 1000자까지 입력 가능합니다.");
         $(this).val(content.substring(0, 1000));
+        content = content.substring(0, 1000); // 글자 수를 1000자로 제한
     }
+   
     $('#cnt').html(content.length + "자");
 });
 
-$('#freeContent').on('keydown', function() {
+
+$('#freeContent').on('keyup', function() {
     var content = $(this).val();
     if (content.length > 1000) {
         alert("최대 1000자까지 입력 가능합니다.");
         $(this).val(content.substring(0, 1000));
+        content = content.substring(0, 1000); // 글자 수를 1000자로 제한
     }
+   
     $('#cnt').html(content.length + "자");
 });
