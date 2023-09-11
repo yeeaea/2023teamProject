15c0eba79e0ddbc0c90fd92aca55786208d1ac2c
we1comerain <we1comerain@kakao.com>
2023-09-11 09:32:23 +0900

    Merge branch 'master' of https://github.com/yeeaea/2023teamProject.git
    왜 자꾸 이러니...?

--- src/main/java/com/pet/free/controller/FreeBoardApiController.java
+++ src/main/java/com/pet/free/controller/FreeBoardApiController.java
@@ -30,32 +30,27 @@ public class FreeBoardApiController {
 	private final FreeBoardService freeBoardService;
 
 	@PostMapping("/api/freeboards")
-	public ResponseEntity<FreeBoard> addFreeBoard(
-		    @RequestParam("freeTitle") String freeTitle,
-		    @RequestParam("freeContent") String freeContent,
-		    @RequestParam(value="file", required = false) MultipartFile file) throws Exception {
-		    FreeBoardRequest request = new FreeBoardRequest();
-		    request.setFreeTitle(freeTitle);
-		    request.setFreeContent(freeContent);
+	public ResponseEntity<FreeBoard> addFreeBoard(@RequestParam("freeTitle") String freeTitle,
+			@RequestParam("freeContent") String freeContent,
+			@RequestParam(value = "file", required = false) MultipartFile file) throws Exception {
+		FreeBoardRequest request = new FreeBoardRequest();
+		request.setFreeTitle(freeTitle);
+		request.setFreeContent(freeContent);
 
-		    FreeBoard savedFreeBoard = freeBoardService.save(request, file);
-		    
-		    // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
-		    return ResponseEntity.status(HttpStatus.CREATED)
-		            .body(savedFreeBoard);
-		}
+		FreeBoard savedFreeBoard = freeBoardService.save(request, file);
+
+		// 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedFreeBoard);
+	}
+
 	// 글 목록 가져오기
 	@GetMapping("/api/freeboards")
-	public ResponseEntity<List<FreeBoardResponse>> findAllQuestions(){
-		List<FreeBoardResponse> freeboard = freeBoardService.findAll(null)
-				.stream()
-				.map(FreeBoardResponse::new)
-				.toList();  // 최종 리턴값을 List로 바꿔라
+	public ResponseEntity<List<FreeBoardResponse>> findAllQuestions() {
+		List<FreeBoardResponse> freeboard = freeBoardService.findAll(null).stream().map(FreeBoardResponse::new)
+				.toList(); // 최종 리턴값을 List로 바꿔라
 		return ResponseEntity.ok().body(freeboard);
 	}
-	
-	
-	
+
 	// 글 하나 조회하기
 	@GetMapping("/api/freeboards/{freeNo}")
 	public ResponseEntity<FreeBoardResponse> findFreeBoard(@PathVariable Long freeNo) {
@@ -63,7 +58,7 @@ public class FreeBoardApiController {
 
 		return ResponseEntity.ok().body(new FreeBoardResponse(freeBoard));
 	}
-	
+
 	// 글 하나 삭제하기
 	@DeleteMapping("/api/freeboards/{freeNo}")
 	public ResponseEntity<Void> deleteFreeBoard(@PathVariable Long freeNo) {
@@ -71,20 +66,18 @@ public class FreeBoardApiController {
 
 		return ResponseEntity.ok().build();
 	}
-	
+
 	// 수정
-		 @PutMapping("/api/freeboards/{freeNo}")
-		    public ResponseEntity<FreeBoard> updatefreeBoard(
-		            @PathVariable long freeNo,
-		            @RequestParam("freeTitle") String freeTitle,
-		            @RequestParam("freeContent") String freeContent,
-		            @RequestParam(name = "file", required = false) MultipartFile file) throws IOException {
+	@PutMapping("/api/freeboards/{freeNo}")
+	public ResponseEntity<FreeBoard> updatefreeBoard(@PathVariable long freeNo,
+			@RequestParam("freeTitle") String freeTitle, @RequestParam("freeContent") String freeContent,
+			@RequestParam(name = "file", required = false) MultipartFile file) throws IOException {
 
-		        // quesNo와 다른 요청 매개변수(quesTitle, quesContent, file)를 사용하여 수정 작업을 수행합니다.
-		        // 이후 ResponseEntity를 사용하여 응답을 반환합니다.
-		        
-		        FreeBoard updatedFreeBoard = freeBoardService.update(freeNo, freeTitle, freeContent, file);
+		// quesNo와 다른 요청 매개변수(quesTitle, quesContent, file)를 사용하여 수정 작업을 수행합니다.
+		// 이후 ResponseEntity를 사용하여 응답을 반환합니다.
 
-		        return ResponseEntity.ok().body(updatedFreeBoard);
-		    }
-		}
+		FreeBoard updatedFreeBoard = freeBoardService.update(freeNo, freeTitle, freeContent, file);
+
+		return ResponseEntity.ok().body(updatedFreeBoard);
+	}
+}
