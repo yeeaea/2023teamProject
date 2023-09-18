25d2b218993f82970706835b55f07e264be82977
Sim-MinA <tlaalsdk7@naver.com>
2023-09-18 14:07:55 +0900

    Merge branch 'master' of https://github.com/yeeaea/2023teamProject.git

--- src/main/resources/static/js/contentCount.js
+++ src/main/resources/static/js/contentCount.js
@@ -1,15 +1,18 @@
 // 글 작성하기에서 내용 2000자 못넘게
-$(document).ready(function() {
-	$('#quesContent').keyup(function(){
-	  var content = $(this).val();
-	  $('.count span').html(content.length);
-	  if (content.length > 1000){
-	    alert("최대 1000자까지 입력 가능합니다.");
-	    $(this).val(content.substring(0, 1000)); // 2000자까지만 입력값으로 설정
-	    $('.count span').html(1000);
-	  }
-	});
-});
+function countingLength(inputElementId, counterElementId) {
+	let quesInputElement = document.getElementById(inputElementId);
+	let quesCounter = document.getElementById(counterElementId);
+
+	if (quesInputElement.value.length > 1000) {
+		alert('댓글을 1000자 이하로 입력해 주세요.');
+		quesInputElement.value = quesInputElement.value.substring(0, 1000);
+		quesInputElement.focus();
+	}
+
+	quesCounter.innerText = quesInputElement.value.length + '/1000자';
+}
+
+
 
 $(document).ready(function() {
 	$('#freeContent').keyup(function(){
