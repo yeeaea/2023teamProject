166d99c00c53fb45a73e8e919948dc4f6050686b
Sim-MinA <tlaalsdk7@naver.com>
2023-09-18 14:33:00 +0900

    글 작성하기에서 글자수 카운팅, 새글 작성하기에서 제목 가운데 정렬

--- src/main/resources/static/js/contentCount.js
+++ src/main/resources/static/js/contentCount.js
@@ -1,27 +1,13 @@
 // 글 작성하기에서 내용 2000자 못넘게
 function countingLength(inputElementId, counterElementId) {
-	let quesInputElement = document.getElementById(inputElementId);
-	let quesCounter = document.getElementById(counterElementId);
+	let wordInputElement = document.getElementById(inputElementId);
+	let wordCounter = document.getElementById(counterElementId);
 
-	if (quesInputElement.value.length > 1000) {
-		alert('댓글을 1000자 이하로 입력해 주세요.');
-		quesInputElement.value = quesInputElement.value.substring(0, 1000);
-		quesInputElement.focus();
+	if (wordInputElement.value.length > 1000) {
+		alert('최대 1000자까지 입력 가능합니다.');
+		wordInputElement.value = wordInputElement.value.substring(0, 1000);
+		wordInputElement.focus();
 	}
 
-	quesCounter.innerText = quesInputElement.value.length + '/1000자';
+	wordCounter.innerText = wordInputElement.value.length + '/1000자';
 }
-
-
-
-$(document).ready(function() {
-	$('#freeContent').keyup(function(){
-	  var content = $(this).val();
-	  $('.count span').html(content.length);
-	  if (content.length > 1000){
-	    alert("최대 1000자까지 입력 가능합니다.");
-	    $(this).val(content.substring(0, 1000)); // 2000자까지만 입력값으로 설정
-	    $('.count span').html(1000);
-	  }
-	});
-});
